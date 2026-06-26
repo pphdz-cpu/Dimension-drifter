7cf42e84f2dbd64a84853a0db592cbec4889fcd6
pphdz-cpu <pphdzg@gmail.com>
2026-06-25 20:23:32 -0700

    Update main.js

--- dimension-drifter/js/main.js
+++ dimension-drifter/js/main.js
@@ -1,26 +1,76 @@
 import { LEVEL_1, renderBoard } from "./grid.js";
+
 import { setupPlayerControls } from "./entities.js";
+
 import { setupRuneControls } from "./runes.js";
+
+
 
 /** @typedef {{ level: number[][], player: { row: number, col: number }, activeRune: string | null, won: boolean }} GameState */
 
+
+
 /** @type {GameState} */
+
 export const gameState = {
+
   level: LEVEL_1,
+
   player: { row: 1, col: 1 },
+
   activeRune: null,
+
   won: false,
+
 };
 
-function initGame() {
-  const board = document.getElementById("board");
-  if (!board) {
-    return;
-  }
 
-  renderBoard(board, gameState.level, gameState.player, gameState.activeRune);
-  setupRuneControls(gameState, board);
-  setupPlayerControls(gameState, board);
+
+/**
+
+ * @param {HTMLElement} boardElement
+
+ */
+
+export function refreshBoard(boardElement) {
+
+  renderBoard(
+
+    boardElement,
+
+    gameState.level,
+
+    gameState.player,
+
+    gameState.activeRune
+
+  );
+
 }
 
+
+
+function initGame() {
+
+  const board = document.getElementById("board");
+
+  if (!board) {
+
+    return;
+
+  }
+
+
+
+  refreshBoard(board);
+
+  setupRuneControls(gameState, () => refreshBoard(board));
+
+  setupPlayerControls(gameState, board);
+
+}
+
+
+
 document.addEventListener("DOMContentLoaded", initGame);
+
